eac042beffc7320ae4ee5aa0b366b6526b311618
Sam Gimbel <me@samgimbel.com>
2011-07-03 18:13:49 -0400

    added post scaffold

---
+++ db/migrate/20110703221323_create_posts.rb
@@ -0,0 +1,22 @@
+class CreatePosts < ActiveRecord::Migration
+  def self.up
+    create_table :posts do |t|
+      t.integer :id
+      t.string :title
+      t.stringpublishdate :createdate
+      t.string :image
+      t.string :platform
+      t.string :icon
+      t.string :link
+      t.integer :tagid
+      t.string :tag
+      t.string :size
+
+      t.timestamps
+    end
+  end
+
+  def self.down
+    drop_table :posts
+  end
+end
