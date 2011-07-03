d23b8859e5bcc648bc6abc468db8c6a68f5f91fa
Sam Gimbel <me@samgimbel.com>
2011-07-03 17:17:09 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Stella::Application.config.secret_token = 'ba434f793260933b41e533b8ff9e81e7aaf9bc81c1ef1c6b11c9b75bfa6241857f209b16256cadb4f0428842b700362a2ccd7b19678437052db6d2d5da035337'
