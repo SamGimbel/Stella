ae2e1440ab04b15fa470e30cb420a4a3e4c86d94
Sam Gimbel <me@samgimbel.com>
2011-07-11 14:07:06 -0400

    added validation

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,8 @@
 class Post < ActiveRecord::Base
+  validates :title,       :presence => true,
+                          :length => { :minimum => 5 }
+  validates :createdate,  :presence => true
+  validates :image,       :presence => true
+  validates :platform,    :presence => true
+  validates :image,       :presence => true
 end
